da8efa77dcb0b40e76dbb3998e4a74fb3517c100
Ferran <fbiayna>
2020-12-02 21:10:01 +0100

    some bugs -ts

--- server/src/routes/adminRouter.js
+++ server/src/routes/adminRouter.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express')
 // const AdminController = require('../controllers/AdminController');
 // const EstablishmentController = require('../controllers/EstablishmentController');
-const PromotionsController = require('../controllers/PromotionsController');
+const PromotionsController = require('../controllers/PromotionsController')
 // const PromotionController = require('../controllers/PromotionController');
 
-function adminRouter(Users, Establishments, Promotions) {
-  const router = express.Router();
+function adminRouter (Users, Establishments, Promotions) {
+  const router = express.Router()
   //   const admin = AdminController(Users, Establishments, Promotions);
   //   const establishment = EstablishmentController(Users, Establishments);
-  const promotions = PromotionsController(Users, Promotions);
+  const promotions = PromotionsController(Users, Promotions)
   //   const promotion = PromotionController(Users, Promotions);
 
   //   router.route('/')
@@ -21,7 +21,7 @@ function adminRouter(Users, Establishments, Promotions) {
   //     .put(establishment.putMethod);
 
   router.route('/promotions')
-    .get(promotions.getMethod);
+    .get(promotions.getMethod)
   // .post(promotions.postMethod);
 
   //   router.route('/promotions/:promotionId')
@@ -29,7 +29,7 @@ function adminRouter(Users, Establishments, Promotions) {
   //     .put(promotion.putMethod)
   //     .delete(promotion.deleteMethod);
 
-  return router;
+  return router
 }
 
-module.exports = adminRouter;
+module.exports = adminRouter
