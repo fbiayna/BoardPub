83a3c28f7861a9fe05180559603655169cca3c73
Ferran <fbiayna>
2020-12-02 20:04:47 +0100

    refactoring login

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,17 +1,16 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native'
-
-const background = { uri: 'https://trello-attachments.s3.amazonaws.com/5ec457ae0da99b76dcdba46b/5ec457af3dca9f5428a9f179/b713ff3791a65ab0ca93ae3e5f76f476/menu-609364_1280.jpg' }
-const logo = { uri: 'https://trello-attachments.s3.amazonaws.com/5ec457ae0da99b76dcdba46b/5ec457af3dca9f5428a9f179/263e7f5a2d4e919205f4745afbf1a032/logo-boardpub.png' }
+import { ImageBackground, Text, View, Image } from 'react-native'
+import styles from '../style/Login'
+import { logoBoardPub, loginBackground } from '../../utils/images'
 
 export default function Login () {
   return (
     <View style={styles.container}>
-      <ImageBackground source={background} style={styles.backimage}>
+      <ImageBackground source={loginBackground()} style={styles.backimage}>
         <View style={styles.shadow}>
           <View style={styles.logo}>
-            <Image source={logo} style={styles.image} />
+            <Image source={logoBoardPub()} style={styles.image} />
           </View>
           <View style={styles.textSlogan}>
             <Text style={styles.text}>Las mejores promociones de tus locales hosteleros, en una sola app.</Text>
@@ -34,99 +33,3 @@ export default function Login () {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  backimage: {
-    opacity: 0.8,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    justifyContent: 'center'
-  },
-  shadow: {
-    width: '100%',
-    height: '100%',
-    opacity: 1,
-    backgroundColor: '#000'
-  },
-  logo: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 2.5
-  },
-  image: {
-    width: 320,
-    height: 100
-  },
-  textSlogan: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textAction: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  text: {
-    width: 250,
-    fontWeight: 'bold',
-    alignItems: 'center',
-    fontSize: 20,
-    lineHeight: 30,
-    textAlign: 'center',
-    color: '#fff'
-  },
-  textSearch: {
-    flex: 1,
-    fontWeight: 'bold',
-    fontSize: 25,
-    lineHeight: 30,
-    textAlign: 'center',
-    color: '#fff'
-  },
-  user: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  buttonUser: {
-    width: 260,
-    height: 60,
-    backgroundColor: '#92000A',
-    justifyContent: 'center',
-    borderRadius: 50,
-    elevation: 4
-  },
-  textUser: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    lineHeight: 22,
-    textAlign: 'center',
-    color: '#fff'
-  },
-  admin: {
-    flex: 2,
-    alignItems: 'center'
-  },
-  buttonAdmin: {
-    width: 260,
-    height: 60,
-    backgroundColor: '#1565C0',
-    justifyContent: 'center',
-    borderRadius: 50,
-    elevation: 4
-  },
-  textAdmin: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    lineHeight: 22,
-    textAlign: 'center',
-    color: '#fff'
-  }
-})
--- client/src/utils/images.ts
+++ client/src/utils/images.ts
@@ -3,3 +3,15 @@ export function meal ():any {
     { uri: 'https://i1.wp.com/catalinacevedomejia.com/wp-content/uploads/2015/10/salmon-518032_1280.jpg' }
   )
 }
+
+export function loading ():any {
+  return ({ uri: 'https://trello-attachments.s3.amazonaws.com/5ec457af3dca9f5428a9f179/210x374/aba40d5ffeb52b3299e1abb01132a636/BoardPub_-_Loading.gif' })
+}
+
+export function loginBackground ():any {
+  return ({ uri: 'https://trello-attachments.s3.amazonaws.com/5ec457ae0da99b76dcdba46b/5ec457af3dca9f5428a9f179/b713ff3791a65ab0ca93ae3e5f76f476/menu-609364_1280.jpg' })
+}
+
+export function logoBoardPub ():any {
+  return ({ uri: 'https://trello-attachments.s3.amazonaws.com/5ec457ae0da99b76dcdba46b/5ec457af3dca9f5428a9f179/263e7f5a2d4e919205f4745afbf1a032/logo-boardpub.png' })
+}
