72f97c85b224887db7149edb73726dea2c36b887
Ferran <fbiayna>
2020-12-08 13:08:38 +0100

    profile compo -done

--- client/src/components/styles/Profile.ts
+++ client/src/components/styles/Profile.ts
@@ -29,17 +29,114 @@ const style = StyleSheet.create({
     fontWeight: 'bold'
   },
   photoContainer: {
-    flex: 1,
+    height: 90,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    elevation: 5,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginBottom: 10
   },
   photoPreContainer: {
     height: 70,
-    width: 70
+    width: 70,
+    borderRadius: 50,
+    borderColor: '#F1F1F1',
+    borderWidth: 3,
+    elevation: 5
   },
   photo: {
     width: '100%',
     height: '100%'
+  },
+  allDescriptionContainer: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  descriptionContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginBottom: 10
+  },
+  infoProfile: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    marginBottom: 5
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  title: {
+    color: '#000',
+    fontSize: 12,
+    paddingRight: 5
+  },
+  description: {
+    color: '#9E9E9E',
+    fontWeight: 'bold'
+  },
+  adminContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10
+  },
+  admin: {
+    color: '#9E9E9E',
+    fontWeight: 'bold'
+  },
+  skylabContainer: {
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5F5F5'
+  },
+  skylabPreContainer: {
+    height: 35,
+    width: 35,
+    borderRadius: 50,
+    borderColor: '#F1F1F1',
+    borderWidth: 3
+  },
+  skylab: {
+    width: '100%',
+    height: '100%'
+  },
+  footerText: {
+    fontSize: 12
+  },
+  signOutContainerTotal: {
+    height: 75,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginBottom: 10
+  },
+  signOutContainer: {
+    flex: 0.5,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  signOut: {
+    height: 40,
+    width: 200,
+    backgroundColor: '#1565C0',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  signOutText: {
+    color: '#fff',
+    fontWeight: 'bold'
   }
 })
 
--- client/src/components/user/Profile.tsx
+++ client/src/components/user/Profile.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
 import style from '../styles/Profile'
-import { View, Text, Button, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity, ScrollView } from 'react-native'
 import firebase from 'firebase'
 import { connect } from 'react-redux'
-import { LoginReducer } from 'utils/interfaces'
+import { LoginReducer } from '../../utils/interfaces'
+import { skylabLogo } from '../../utils/images'
 
 function Profile ({ user }:LoginReducer) {
   return (
@@ -14,23 +15,56 @@ function Profile ({ user }:LoginReducer) {
             <Text style={style.profileText}>Tu Perfil</Text>
         </View>
       </View>
-      <Button title='Sign out' onPress={() => firebase.auth().signOut()}/>
-      <View style={style.photoContainer}>
-        <View style={style.photoPreContainer}>
-          <ImageBackground source={{ uri: user?.photo }} style={style.photo} imageStyle={{ borderRadius: 50 }}></ImageBackground>
+      <ScrollView>
+        <View style={style.photoContainer}>
+          <View style={style.photoPreContainer}>
+            <ImageBackground source={{ uri: user?.photo }} style={style.photo} imageStyle={{ borderRadius: 50 }}></ImageBackground>
+          </View>
         </View>
-      </View>
-      <Text>{user?.name}</Text>
-      <Text>{user?.surname}</Text>
-      <Text>{user?.email}</Text>
-      <Text>{user?.admin ? 'El perfil es administrador' : 'El perfil no es administrador'}</Text>
-      <Text>{user?.favorites}</Text>
+        <View style={style.descriptionContainer}>
+          <Text style={style.infoProfile}>INFORMACIÓN DEL USUARIO</Text>
+          <View style={style.allDescriptionContainer}>
+            <View style={style.textContainer}>
+              <Text style={style.title}>Nombre:</Text>
+              <Text style={style.description}>{user?.name}</Text>
+            </View>
+            <View style={style.textContainer}>
+              <Text style={style.title}>Apellidos:</Text>
+              <Text style={style.description}>{user?.surname}</Text>
+            </View>
+            <View style={style.textContainer}>
+              <Text style={style.title}>Email:</Text>
+              <Text style={style.description}>{user?.email}</Text>
+            </View>
+            {user?.favorites?.length
+              ? <View style={style.textContainer}>
+                <Text style={style.title}>Favoritos:</Text>
+                <Text style={style.description}>{user?.favorites}</Text>
+                </View>
+              : null}
+          </View>
+          {user?.admin ? <View style={style.adminContainer}><Text style={style.admin}>Tu perfil es administrador</Text></View> : <View style={style.adminContainer}><Text style={style.admin}>Tu perfil no es administrador</Text></View>}
+        </View>
+        <View style={style.signOutContainerTotal}>
+          <View style={style.signOutContainer}>
+            <TouchableOpacity style={style.signOut} onPress={() => firebase.auth().signOut()} activeOpacity={0.8}>
+              <Text style={style.signOutText}>Cerrar Sesión</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={style.skylabContainer}>
+          <View style={style.skylabPreContainer}>
+            <ImageBackground source={skylabLogo()} style={style.skylab} imageStyle={{ borderRadius: 50 }}></ImageBackground>
+          </View>
+          <Text style={style.footerText}>Skylab Coders Academy</Text>
+          <Text style={style.footerText}>2020-2021</Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 function mapStateToProps ({ loginReducer }: any) {
-  console.log(loginReducer.user.surname)
   return {
     user: loginReducer.user,
     userState: loginReducer.userState
