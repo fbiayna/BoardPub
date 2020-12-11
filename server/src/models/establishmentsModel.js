6a8f582351eeaebb80f30aa7147c8c7b65e40144
Ferran <fbiayna>
2020-12-11 12:28:07 +0100

    model changes

--- server/src/models/establishmentsModel.js
+++ server/src/models/establishmentsModel.js
@@ -5,6 +5,7 @@ const { Schema, model } = mongoose;
 const establishmentsSchema = new Schema({
   name: { type: String },
   ubication: { type: String },
+  coords: { latitude: { type: Number }, longitude: { type: Number } },
   city: { type: String },
   photo: { type: String },
   description: { type: String },
