81acb98c07cbabfa8e8cd02cf70a5dcda21f0b20
Ferran Biayna <67982134+fbiayna@users.noreply.github.com>
2020-12-22 14:40:12 +0100

    Delete dbUrl.ts

--- server/src/utils/dbUrl.ts
+++
@@ -1,7 +0,0 @@
-function mongoUrl ():string {
-  return (
-    'mongodb+srv://fbiayna:zaOaUtHdaX3c5wA8@cluster0.bsyq9.mongodb.net/productDBv1?retryWrites=true&w=majority'
-  )
-}
-
-module.exports = mongoUrl
